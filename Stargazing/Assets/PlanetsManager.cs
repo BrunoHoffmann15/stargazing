e9baae4e46176b0710a5044523020b8cc0108435
brunoshoffmann <bruno.siqueira.hoffmann@gmail.com>
2025-05-27 23:49:10 -0300

    feat: add moon

---
+++ Stargazing/Assets/PlanetsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetsManager
+{
+    public Vector3 getPlanetPositionByName(String planetName)
+    {
+        var vector = new Vector3();
+
+        vector.x = 0.02f;
+        vector.y = 0.03f;
+        vector.z = 0.4f;
+
+        return vector;
+ 
+    }
+}
